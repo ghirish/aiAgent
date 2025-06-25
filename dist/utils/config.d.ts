e265e27412fb21b75114ea0bb0bd0217ebb8fd67
Ghirish Thaenraj <ghirish05@gmail.com>
2025-06-24 17:27:03 -0700

    cleaning up the repository

--- dist/utils/config.d.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from '../types/index.js';
-export declare function loadConfig(): Config;
-export declare function validateEnvironment(): {
-    isValid: boolean;
-    missing: string[];
-};
-export declare function getConfig(): Config;
